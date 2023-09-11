a10f0b47815299d664cf894213a14c02b18158e6
jesse <jessefortuin850@gmail.com>
2023-09-11 15:16:48 +0200

    Feat: Removed bin and vs, added .gitignore

--- FairwayDrivingRange.Api/FairwayDrivingRange.Infrastructure/Specification.cs
+++ FairwayDrivingRange.Api/FairwayDrivingRange.Infrastructure/Specification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ardalis.Specification;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 //TODO: implement specification pattern http://specification.ardalis.com/extensions/extend-define-evaluators.html
 namespace FairwayDrivingRange.Infrastructure
 {
-    public abstract class Specification<T> : ISpecification<T>
+    public class Specification <T>
     {
         public bool IsSatisfied(T obj)
         {
